13d7822f6b7368cebcaf98110a990dd9d9909628
ernestguevarra <ernestgmd@gmail.com>
2019-04-25 13:29:28 +0100

    create package function

---
+++ R/nutribudgetr.R
@@ -0,0 +1,21 @@
+################################################################################
+#
+#' nutribudgetr
+#'
+#' An Implementation of SPRING's Nutrition Budget Analysis Tool in R
+#' 
+#' Despite recognition that creating and sustaining financial support for 
+#' nutrition is critical, nutrition advocates lack access to accurate and timely 
+#' financial data. In response to this challenge, the USAID-funded Strengthening 
+#' Partnerships, Results, and Innovations in Nutrition Globally (SPRING) project 
+#' has developed a tool that will help nutrition stakeholders learn where the 
+#' funding is, understand existing resource gaps, and advocate for adequate 
+#' budgets. This package is an implementation of SPRING's budget analysis tool 
+#' in R.
+#'
+#' @docType package
+#' @name nutribudgetr
+#'
+#
+################################################################################
+NULL
